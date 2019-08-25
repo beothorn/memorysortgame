b00f241fde4d072f9e79d3319b25e3b3268e8768
lucass <lucass@gmail.com>
2019-08-25 18:13:16 +0200

    dragging item

--- code.js
+++ code.js
@@ -1,49 +1,64 @@
 
-
-function handleDragOver(e) {
-  if (e.preventDefault) {
-    e.preventDefault(); // Necessary. Allows us to drop.
-  }
-
-  e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
-
-  return false;
-}
-
-
-
-
-
-Array.from(document.getElementsByClassName("item")).forEach( item => {item.addEventListener('dragstart', function(e) {
-    this.style.opacity = '0.4';
-}, false)});
-
-
-
-Array.from(document.getElementsByClassName("item-start-hidden")).forEach( item => {
-    
-    item.addEventListener('dragenter', function(e) {
-      this.classList.remove('item-start-hidden');
-      this.classList.add('item-start');
+Array.from(document.getElementsByClassName("col")).forEach( col => {
+    col.addEventListener('dragstart', function(e) {
+      this.classList.remove('col');
+      this.classList.add('col-drag');
+      let item = this.children[0]
+      item.classList.remove('item-hidden');
+      item.classList.add('item');
     }, false)
 
-    item.addEventListener('dragleave', function(e) {
-      this.classList.remove('item-start');
-      this.classList.add('item-start-hidden');
+    col.addEventListener('dragend', function(e) {
+      this.classList.add('col');
+      this.classList.remove('col-drag');
+      let item = this.children[0]
+      item.classList.remove('item');
+      item.classList.add('item-hidden');
     }, false)
-    
+
+    col.addEventListener('dragover', function(e) {
+      if (e.preventDefault) {
+        e.preventDefault(); // Necessary. Allows us to drop.
+      }
+
+      e.dataTransfer.dropEffect = 'move';  // See the section on the DataTransfer object.
+
+      //var dragIcon = document.createElement('img');
+      //dragIcon.src = 'logo.png';
+      //dragIcon.width = 100;
+      //let x = e.dataTransfer
+      //x.setDragImage(dragIcon, -10, -10);
+
+      return false;
+    }  , false)
 });
 
-Array.from(document.getElementsByClassName("item-end-hidden")).forEach( item => {
+
+
+Array.from(document.getElementsByClassName("col-start-hidden")).forEach( col => {
     
-    item.addEventListener('dragenter', function(e) {
-      this.classList.remove('item-end-hidden');
-      this.classList.add('item-end');
+    col.addEventListener('dragenter', function(e) {
+      this.classList.remove('col-start-hidden');
+      this.classList.add('col-start');
     }, false)
 
-    item.addEventListener('dragleave', function(e) {
-      this.classList.remove('item-end');
-      this.classList.add('item-end-hidden');
+    col.addEventListener('dragleave', function(e) {
+      this.classList.remove('col-start');
+      this.classList.add('col-start-hidden');
+    }, false)
+
+});
+
+Array.from(document.getElementsByClassName("col-end-hidden")).forEach( col => {
+    
+    col.addEventListener('dragenter', function(e) {
+      this.classList.remove('col-end-hidden');
+      this.classList.add('col-end');
+    }, false)
+
+    col.addEventListener('dragleave', function(e) {
+      this.classList.remove('col-end');
+      this.classList.add('col-end-hidden');
     }, false)
     
 });
